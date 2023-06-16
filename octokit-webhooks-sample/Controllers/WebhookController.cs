8484839570f20e60b00bbc1e318a761e90080f63
Zookerman3 <szook7@gmail.com>
2023-06-16 15:13:27 -0500

    it works!

---
+++ octokit-webhooks-sample/Controllers/WebhookController.cs
@@ -0,0 +1,49 @@
+using CommitScraper.Models;
+using Microsoft.AspNetCore.Mvc;
+using Octokit;
+
+namespace CommitScraper.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WebHookController : ControllerBase
+{
+    private readonly MyWebhookEventProcessor _processor;
+    private readonly GitHubClient _gitHubClient;
+    private readonly ILogger<WebHookController> _logger;
+    public WebHookController(MyWebhookEventProcessor processor, GitHubClient gitHubClient, IConfiguration configuration, ILogger<WebHookController> logger)
+    {
+        string appId = configuration["APP_ID"];
+        string webhookSecret = configuration["WEBHOOK_SECRET"];
+        string privateKeyPath = configuration["PRIVATE_KEY_PATH"];
+        var gitHubToken = configuration["GITHUB_TOKEN"];
+
+        _processor = processor;
+
+        _gitHubClient = new GitHubClient(new ProductHeaderValue("CommitScraper"))
+        {
+            Credentials = new Credentials(gitHubToken)
+        };
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ProcessCommit([FromBody] PayloadModel payload)
+    {
+        foreach (var commit in payload.Commits)
+        {
+            var commitSHA = commit.Id;
+            var commitDetails = await _gitHubClient.Repository.Commit.Get(payload.Repository.Owner.Login, payload.Repository.Name, commitSHA);
+
+            var commitMessage = commitDetails.Commit.Message;
+            var committer = commitDetails.Commit.Committer;
+
+            _logger.LogInformation($"Committer: {committer.Name}");
+            _logger.LogInformation($"Commit message: {commitMessage}");
+        }
+        return Ok();
+    }
+}
+
+
+
---
+++ octokit-webhooks-sample/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.Configuration;
+using Octokit;
+using System;
+using Octokit.Webhooks.AspNetCore;
+using Octokit.Webhooks.AzureFunctions;
+using System.Text;
+using Octokit.Webhooks;
+using Microsoft.AspNetCore.Builder;
+using CommitScraper;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<WebhookEventProcessor, MyWebhookEventProcessor>();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+{
+  endpoints.MapGitHubWebhooks();
+});
+
+app.Run();
